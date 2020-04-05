ead0a90e60618cbe688ed2bb4149b969b7b83305
sungwon.cha <mygenie3@naver.com>
2020-04-05 18:53:44 +0900

    fix

---
+++ packages/gatsby-plugin-bluewings/src/pages/404.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Styled } from 'theme-ui';
+
+const NotFound = () => (
+  <div
+    style={{
+      display: 'flex',
+      alignItem: 'center',
+      justifyContent: 'center',
+    }}
+  >
+    <Styled.h1>Page Not Found</Styled.h1>
+  </div>
+);
+
+export default NotFound;
